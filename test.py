0553fd0147354afd7e7af9970776791b7fa5d039
hainamnguyen <hainamnguyen.th.vn@gmail.com>
2021-03-10 16:23:18 +0700

    add agnostic

--- test.py
+++ test.py
@@ -120,7 +120,8 @@ def test(data,
             targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
             lb = [targets[targets[:, 0] == i, 1:] for i in range(nb)] if save_hybrid else []  # for autolabelling
             t = time_synchronized()
-            out = non_max_suppression(out, conf_thres=conf_thres, iou_thres=iou_thres, labels=lb, multi_label=True)
+            # out = non_max_suppression(out, conf_thres=conf_thres, iou_thres=iou_thres, labels=lb, multi_label=True)
+            out = non_max_suppression(out, conf_thres=conf_thres, iou_thres=iou_thres, labels=lb, multi_label=True, agnostic= False)
             t1 += time_synchronized() - t
 
         # Statistics per image
